d2cdfc8a1fdea14bffae258f4ae46af68bb56f0e
Akshaysharma98 <58226752+Akshaysharma98@users.noreply.github.com>
2020-06-05 18:58:36 +0530

    homepage done

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -36,57 +39,187 @@ class _HomePageState extends State<HomePage> {
                 ),
             ),
 
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                scrollDirection: Axis.vertical,
-                primary: false,
-                children: <Widget>[
-                  FlatButton(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Services",
-                        style: TextStyle(
-                        color:Colors.black ,
-                         fontSize: 20),
-                         ),
-                         
-                      ],
-                    ),
-                    ),
-                  FlatButton(
-                    
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Packages",
-                        style: TextStyle(
-                        color:Colors.black ,
-                         fontSize: 20),
-                         ),
-                         
-                      ],
-                    ),
-                    ),
-                    FlatButton(
-                    
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Feedback",
-                        style: TextStyle(
-                        color:Colors.black ,
-                         fontSize: 20),
-                         ),
-                         
-                      ],
-                    ),
-                    ),
-                  
-                ],
+            Positioned(
+              top: 100,
+              left:180,
+              right: 50,
+                child: Container(
+                height: MediaQuery.of(context).size.height *0.28,
+                decoration: BoxDecoration(
+                  color: Colors.green[200],
+                  borderRadius: BorderRadius.all(const Radius.circular(10)),
                 ),
+                
+                child: Stack(
+                  children: <Widget> [
+
+                    Positioned(
+                      top: 90,
+                      left:12,                      
+                      child: Text('Services',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 30,
+                      left: 20,
+                      right: 20,
+                        child: Container(
+                        child: Image.asset('Images/car_parts.png',
+                        height: 50,
+                        width: 60,
+                        
+                        ),
+                      ),
+                    )
+
+
+                  ],
+                ),
+                
+              ),
             ),
+
+            Positioned(
+              top: 100,
+              left:30,
+              right: 200,
+                child: Container(
+                height: MediaQuery.of(context).size.height *0.13,
+                // color: Colors.blue,
+                decoration: BoxDecoration(
+                  color: Colors.orange[200],
+                  borderRadius: BorderRadius.all(const Radius.circular(10)),
+                ),
+
+                child: Stack(
+                  children: <Widget> [
+
+                    Positioned(
+                      top: 40,
+                      left:14,                      
+                      child: Text('Packages',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 10,
+                      left: 2,
+                      right: 2,
+                        child: Container(
+                        child: Image.asset('Images/pack.png',
+                        height: 24,
+                        width: 26,
+                        
+                        ),
+                      ),
+                    )
+
+
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned(
+              top: 200,
+              left:30,
+              right: 200,
+                child: Container(
+                height: MediaQuery.of(context).size.height *0.13,
+                // color: Colors.blue,
+                decoration: BoxDecoration(
+                  color: Colors.pink[100],
+                  borderRadius: BorderRadius.all(const Radius.circular(10)),
+                ),
+
+                child: Stack(
+                  children: <Widget> [
+
+                    Positioned(
+                      top: 40,
+                      left:14,                      
+                      child: Text('Feedback',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 10,
+                      left: 2,
+                      right: 2,
+                        child: Container(
+                        child: Image.asset('Images/feedback.png',
+                        height: 24,
+                        width: 26,
+                        
+                        ),
+                      ),
+                    )
+
+
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned(
+              top: 300,
+              left:60,
+              child: Text('Our Work',
+              style: TextStyle(
+                fontSize:30,
+                color:Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+              ),
+            ),
+
+            Positioned(
+              top: 350,
+              left: 30,
+              right: 30,
+              child: Container(
+                height: 180.0,
+                width: 300.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(const Radius.circular(10)),
+                ),
+                child: Carousel(
+                  images: [
+                    ExactAssetImage("Images/porche_car_wash.jpg"),
+                    ExactAssetImage("Images/engine_car_wash.jpg"),
+                    ExactAssetImage("Images/audi_car_wash.jpg"),
+                    ExactAssetImage("Images/bmw_car_wash.jpg"),
+                    ExactAssetImage("Images/jaguar_car_wash.jpg"),
+                    ExactAssetImage("Images/Ferrari_car_wash.jpg"),
+                    ExactAssetImage("Images/lam_car_wash.jpg"),
+                    ExactAssetImage("Images/mustang_car_wash.jpg"),
+                  ],
+                      dotSize: 4.0,
+                      dotSpacing: 15.0,
+                      dotColor: Colors.lightGreenAccent,
+                      indicatorBgPadding: 5.0,
+                      dotBgColor: Colors.transparent.withOpacity(0.5),
+                      borderRadius: true,
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
